ed2732ab386602557f00b7a5a975aeaf3d5cc508
christhopher.leao <christhopherleao@icloud.com>
2019-01-10 21:05:11 -0200

    added Algorithms and base logic

--- calculate-order.js
+++ calculate-order.js
@@ -1,12 +1,32 @@
 #!/usr/bin/env node
 // @ts-nocheck
 
-console.log('\nHello World\n\n');
+const csvParser = require('csvtojson');
+const vat = 23;
 
-process.argv.slice(2).forEach((arg, i) => {
-  if (i & 1) { arg = parseInt(arg, 10); }
-  console.log(arg, typeof arg);
+console.info('Welcome to "Order Price Calculator"');
+
+// Order
+const args = process.argv.slice(3);
+const order = [];
+const catalog = await csvParser({ noheader:true }).fromFile(process.argv[2]);
+const itemPricing = index => {
+  // TODO: If a product is out of stock
+  console.error(`Item ${order[index][0]} is out of stock`);
+  process.exitCode = 1;
+
+  // TODO: Calculate Price
+  catalog[index] order[index]
+
+  if (i = 0) return;
+};
+
+args.forEach((arg, i) => {
+  // Checks if argument is even, (Amount) so we can parse to a number.
+  if (!(i & 1)) return;
+  return order.push([order[i-1], arg]);
 });
 
-console.log('\n\nThe end');
+// TODO: Add VAT and output the result
+console.log(`Total: ${total}`);
 process.exit(0);
