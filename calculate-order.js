ad5ced6ab922295740369a5c9061d49449694b22
christhopher.leao <christhopherleao@icloud.com>
2019-01-11 02:32:02 -0200

    refactor to a more functional approach

--- calculate-order.js
+++ calculate-order.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 // @ts-nocheck
-
 const csvParser = require('csvtojson');
 
 const vat = 23;
-const order = [];
 const getCatalog = csvParser({ noheader: true, output: "csv" }).fromFile(process.argv[2]);
-let catalog;
-
-process.argv.slice(3).forEach((arg, i) => {
+const order = process.argv.slice(3).reduce((acc, arg, i) => {
   // Checks if argument is even, (Amount) so we can parse to a number.
-  if ((i > 0) && (i & 1)) order.push(parseInt(arg, 10));
-});
+  if ((i > 0) && (i & 1)) acc.push(parseInt(arg, 10));
+  return acc;
+}, []);
 
-const itemPricing = (amount, i) => {
-  let [item, stock, price] = catalog[i];
+const itemPricing = (amount, catalog) => {
+  let [item, stock, price] = catalog;
   stock = parseInt(stock, 10);
   price = Number(price);
 
@@ -29,8 +26,8 @@ const itemPricing = (amount, i) => {
 };
 
 (async () => {
-  catalog = await getCatalog;
-  const total = order.reduce((acc, amount, index) => acc + itemPricing(amount, index), 0)
+  const catalog = await getCatalog;
+  const total = order.reduce((acc, amount, index) => acc + itemPricing(amount, catalog[index]), 0)
 
   console.log(`Total: ${total + total * (vat / 100)}`);
   process.exit(0);
